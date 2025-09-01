031b156474d207f81d5d86d5ab00920302f91281
macs100 <lu.rgnr@gmail.com>
2025-09-01 00:35:54 -0300

    first steps

---
+++ busqueda binaria/Raab Game I.cpp
@@ -0,0 +1,26 @@
+/**
+ * Time limit: 1.00 s 
+ * Memory limit: 512 MB
+ * 
+ * Consider a two player game where each player has n cards numbered 1,2,\dots,n. 
+ * On each turn both players place one of their cards on the table. The player who placed the higher card gets one point. 
+ * If the cards are equal, neither player gets a point. The game continues until all cards have been played.
+ * 
+ * You are given the number of cards n and the players' scores at the end of the game, a and b. 
+ * Your task is to give an example of how the game could have played out.
+ * 
+ * 
+ * Input
+ * The first line contains one integer t: the number of tests.
+ * Then there are t lines, each with three integers n, a and b.
+ * 
+ * Output:
+ * For each test case print YES if there is a game with the given outcome and NO otherwise.
+ * If the answer is YES, print an example of one possible game. Print two lines representing the order in which the players place their cards. You can give any valid example.
+ * 
+ * 
+ * Constraints
+ * 1 < t < 1000
+ * 1 < n < 100
+ * 0 < a,b < n
+ */
